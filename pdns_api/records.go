5d89a35153500b286576dd9624965270867d0a2c
tamura <tamura@pepabo.com>
2019-09-27 17:45:46 +0900

    update swag

--- pdns_api/records.go
+++ pdns_api/records.go
@@ -56,7 +56,7 @@ func (h *recordHandler) getRecords(c echo.Context) error {
 // @Produce  json
 // @Param id path int true "Record ID "
 // @Param record body model.Record true "Record Object"
-// @Success 200 {object} model.Record
+// @Success 200 "OK"
 // @Failure 403 {object} pdns_api.HTTPError
 // @Failure 404 {object} pdns_api.HTTPError
 // @Failure 500 {object} pdns_api.HTTPError
@@ -91,7 +91,7 @@ func (h *recordHandler) updateRecord(c echo.Context) error {
 // @Produce  json
 // @Param id path int true "Record ID "
 // @Param record body model.Record true "Record Object"
-// @Success 200 {object} model.Record
+// @Success 200 "OK"
 // @Failure 403 {object} pdns_api.HTTPError
 // @Failure 404 {object} pdns_api.HTTPError
 // @Failure 500 {object} pdns_api.HTTPError
@@ -109,7 +109,7 @@ func (h *recordHandler) enableRecord(c echo.Context) error {
 // @Produce  json
 // @Param id path int true "Record ID "
 // @Param record body model.Record true "Record Object"
-// @Success 200 {object} model.Record
+// @Success 200 "OK"
 // @Failure 403 {object} pdns_api.HTTPError
 // @Failure 404 {object} pdns_api.HTTPError
 // @Failure 500 {object} pdns_api.HTTPError
@@ -147,7 +147,7 @@ func changeState(h *recordHandler, c echo.Context, disabled bool) error {
 // @Accept  json
 // @Produce  json
 // @Param id path int true "Record ID "
-// @Success 204 {object} model.Record
+// @Success 204 "No Content"
 // @Failure 403 {object} pdns_api.HTTPError
 // @Failure 404 {object} pdns_api.HTTPError
 // @Failure 500 {object} pdns_api.HTTPError
